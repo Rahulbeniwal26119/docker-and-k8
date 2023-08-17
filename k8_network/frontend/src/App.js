31facbdf1799df61f0b238a238ceaa4e2c62dca3
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2023-08-17 21:03:11 +0530

    adding reverse proxy with ngnix

--- k8_network/frontend/src/App.js
+++ k8_network/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const fetchTasks = useCallback(function () {
-    fetch('http://192.168.99.100:32140/tasks', {
+    fetch('/user-tasks/tasks', {
       headers: {
         'Authorization': 'Bearer abc'
       }
@@ -29,7 +29,7 @@ function App() {
   );
 
   function addTaskHandler(task) {
-    fetch('http://192.168.99.100:32140/tasks', {
+    fetch('/user-tasks/tasks', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
